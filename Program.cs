aad54397782c8f8489e5a0e5354f748b738152d4
naim90 <naim.elmoudni@gmail.com>
2021-10-11 09:46:27 +0200

    euro/dollars_Delegates

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+            Monnaie showmoney = Euro;
+
+            Console.WriteLine($"{x} {showmoney()}");
+
+            showmoney = Dollars;
+
+            Console.WriteLine($"{x} {showmoney()}");
+        }
+
+
+
+
+
+    public delegate string Monnaie();
+
+
+        public static string Euro()
+
+        {
+
+            return "euro";
+
+        }
+
+
+        public static string Dollars()
+
+        {
+
+            return "dollars";
+
+        }
+
+    }
+}
